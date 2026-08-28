361ea5269f9bbc9d9b59725668c9f14214ed06d2
Bashar Eid <bashar@withclad.com>
2026-08-27 20:23:35 -0400

    extract product component

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,13 @@
 export type Link = { href: string; label: string };
+
+export type Product = {
+  name: string;
+  descriptionParagraphs: string[];
+  coverCredit?: string;
+  price: number | string;
+  buttonText?: string;
+  stripeUrl: string;
+  images: string[];
+  isIssue?: boolean;
+  fit?: "cover";
+};
